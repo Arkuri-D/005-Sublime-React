3bdaa62afa5237ced0f23c0de889c1e26f26ffa5
arkuri <arkurideuz@gmail.com>
2024-05-14 14:22:32 -0300

    retomando, submenu con problemas en el css

--- Sublime/src/components/specific/menud1.jsx
+++ Sublime/src/components/specific/menud1.jsx
@@ -3,20 +3,14 @@ import MenuD from '../generics/menues_d';
 
 const Menud1 = () => {
     const ArticulosP = [
-        { elementos: ['Artículos', 'Tazas', 'Remeras', 'Gorras', ['Entrega rápida', 'Envío gratuito', 'Pan con queso'] ] },
+        { elementos: ['Artículos', ['Entrega rápida', 'Envío gratuito', 'Pan con queso', ],  'Tazas', 'Remeras', 'Gorras', ['Entrega rápida', 'Envío gratuito', 'Pan con queso', ] ] },
     ];
-    const ArticulosS = [
-        { elementos: ['Entrega rápida', 'Envío gratuito', 'Pan con queso'] },
-    ];
+    
 
     return (
         <div>
             {ArticulosP.map((elemento, index) => (
                 <MenuD key={index} elementos={elemento.elementos}>
-                    {/* Aquí anidamos otro MenuD dentro de este */}
-                    {ArticulosS.map((subElemento, subIndex) => (
-                        <MenuD key={subIndex} elementos={subElemento.elementos} />
-                    ))}
                 </MenuD>
             ))}
         </div>
