d6c528b4447be3334f18a7ae4250fe3bb5f80af3
arkuri <arkurideuz@gmail.com>
2024-05-15 14:00:54 -0300

    MenuD funcionales, falta mejoras su css

--- Sublime/src/components/generics/menues_d.jsx
+++ Sublime/src/components/generics/menues_d.jsx
@@ -3,20 +3,33 @@ import React from 'react';
 const MenuD = ({ elementos }) => {
     return (
         <div className='MenuD'>
-            <h3>{elementos[0]}</h3>
+            <h3>{elementos[0].texto}</h3>
             <ul>
                 {elementos.slice(1).map((elemento, index) => (
                     <React.Fragment key={index}>
                         {Array.isArray(elemento) ? (
                             <li key={index} className='SubMenuD'>
-                                <ul>{elemento[0]}
+                                {elemento[0].texto}
+                                <ul>
                                     {elemento.slice(1).map((subElemento, subIndex) => (
-                                        <li key={subIndex}>{subElemento}</li>
+                                        <li key={subIndex}>
+                                            {subElemento.url ? (
+                                                <a href={subElemento.url}>{subElemento.texto}</a>
+                                            ) : (
+                                                subElemento.texto
+                                            )}
+                                        </li>
                                     ))}
                                 </ul>
                             </li>
                         ) : (
-                            <li>{elemento}</li>
+                            <li key={index}>
+                                {elemento.url ? (
+                                    <a href={elemento.url}>{elemento.texto}</a>
+                                ) : (
+                                    elemento.texto
+                                )}
+                            </li>
                         )}
                     </React.Fragment>
                 ))}
--- Sublime/src/components/specific/menud1.jsx
+++ Sublime/src/components/specific/menud1.jsx
@@ -3,19 +3,79 @@ import MenuD from '../generics/menues_d';
 
 const Menud1 = () => {
     const ArticulosP = [
-        { elementos: ['Artículos', ['Entrega rápida', 'Envío gratuito', 'Pan con queso', ],  'Tazas', 'Remeras', 'Gorras', ['Entrega rápida', 'Envío gratuito', 'Pan con queso', ] ] },
+        {
+            elementos: [
+                { texto: 'Artículos Personalizables' },
+                [
+                    { texto: 'Ceámica', url: '/ceramica' },
+                    { texto: 'Tazas', url: '/tazas' },
+                    { texto: 'Tazas magicas', url: '/tazas-magicas' },
+                ],
+                [
+                    { texto: 'Madera', url: '/madera' },
+                    { texto: 'Bandejas', url: '/bandejas' },
+                    { texto: 'Carteles', url: '/carteles' },
+                    { texto: 'Rompe Cabezas', url: '/rompe-cabezas' },
+                    { texto: 'Porta Retratos', url: '/porta-retratos' },
+                ],
+                [
+                    { texto: 'Plastico', url: '/plastico' },
+                    { texto: 'Taza de plastico', url: '/taza-de-plastico' },
+                    { texto: 'Cantimploras ', url: '/cantimploras' },
+                    { texto: 'Jarro Térmico', url: '/jarro-termico' },
+                    { texto: 'Mates', url: '/mates' },
+                    { texto: 'Mates Listos', url: '/mates-listos' },
+                    { texto: 'Vaso', url: '/Vaso' },
+                    { texto: 'LLaveros', url: '/llaveros' },
+                ],
+                [
+                    { texto: 'Remeras', url: '/remeras' },
+                    { texto: 'Remera Blanca Unisexs', url: '/remera-blanca-unisex' },
+                    { texto: 'Remera Gris Unisexs', url: '/remera-gris-unisex' },
+                    { texto: 'Remera Blanca Mangas Negras Unisexs', url: '/remera-blanca-mangas-negras-unisex' },
+                    { texto: 'Remera Gris Mangas Negras Unisexs', url: '/remera-gris-mangas-negras-unisex' },
+                    { texto: 'Remera Blanca Niños', url: '/remera-blanca-niños' },
+                ],
+                { texto: 'Vidrio', url: '/vidrio' },
+                { texto: 'Aluminio', url: '/aluminio' },
+            ]
+        }
     ];
-    
+
+        const Combos = [
+            {
+                elementos: [
+                    { texto: 'Combos' },
+                    { texto: 'Arreglos con Dulces', url: '/arreglos-dulces' },
+                    { texto: 'Arreglos para Desayunos', url: '/arreglos-desayunos' },
+                ]
+            }
+        ];
+
+        const TematicaMes = [
+            {
+                elementos: [
+                    { texto: 'Tematica del Mes' },
+                    { texto: 'Sorpresa para Mamá', url: '/sorpresa-mama' },
+                    { texto: 'Descuento Especial', url: '/descuento-especial' },
+                    { texto: 'Regalo Abuelita', url: '/regalo-abuelita' },
+                ]
+            }
+        ];
 
     return (
-        <div>
+        <div className='menucontainer'>
             {ArticulosP.map((elemento, index) => (
-                <MenuD key={index} elementos={elemento.elementos}>
-                </MenuD>
+                <MenuD key={index} elementos={elemento.elementos} />
+            ))}
+            {Combos.map((elemento, index) => (
+                <MenuD key={index} elementos={elemento.elementos} />
+            ))}
+            {TematicaMes.map((elemento, index) => (
+                <MenuD key={index} elementos={elemento.elementos} />
             ))}
         </div>
     );
 };
 
 export default Menud1;
-
